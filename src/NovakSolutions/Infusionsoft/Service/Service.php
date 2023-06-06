68b0db41c929c796c330ab9b6980c3359f4ed06b
Jordan Novak <freezey@gmail.com>
2023-06-06 09:20:33 -0700

    Update Service class to handle 404 response with a specific exception

--- src/NovakSolutions/Infusionsoft/Service/Service.php
+++ src/NovakSolutions/Infusionsoft/Service/Service.php
@@ -40,13 +40,14 @@ class Service
                 throw new ServerErrorException("Infusionsoft may return a 500 if the requested record isn't found. Error Message: " . $result->body, $result->responseCode);
             case 596:
                 throw new BadRequestException("Unknown Service. Error Message: " . $result->body, $result->responseCode);
+            case 404:
+                throw new BadRequestException("Resource not found. Error Message: " . $result->body, $result->responseCode);
             case 401:
                 throw new UnAuthorizedException("Got 401 response from Infusionsoft during call to " . static::$endPoint . " Error Message: " . $result->body, $result->responseCode);
             case 400:
                 throw new BadRequestException("Got Bad Request Exception. Error Message: " . $result->body, $result->responseCode);
-                break;
             default:
-                throw new UnknownResponseException("Got a response I don't know what to do with. ResponseCode: " . $result->responseCode . " Error Message: " . $result->body, $result->responseCode);
+                throw new UnknownResponseException("Unknown responGot a response I don't know what to do with. ResponseCode: " . $result->responseCode . " Error Message: " . $result->body, $result->responseCode);
         }
     }
 
